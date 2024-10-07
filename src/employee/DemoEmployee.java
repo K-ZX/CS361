2d9449b2bfa1a55c6bc0954b77c87f7fcfaa7cbe
K-ZX <163035740+K-ZX@users.noreply.github.com>
2024-10-07 18:35:59 -0400

    Update DemoEmployee.java

--- src/employee/DemoEmployee.java
+++ src/employee/DemoEmployee.java
@@ -8,22 +8,22 @@ public class DemoEmployee {
 
     public static void main(String[] args) {
 
-        // Creating Employee and PartTimeEmployee objects
+        
         Employee emp1 = new Employee("John", "Smith", "123456790");
 
         PartTimeEmployee pEmp1 = new PartTimeEmployee("Jane", "Smith", "123456777", 17.5);
 
-        // Upcasting: PartTimeEmployee to Employee
+        
         Employee emp3 = pEmp1;
         System.out.println("emp3:");
-        emp3.whoAmI(); // Calls whoAmI() from Employee, but polymorphism means the overridden method in PartTimeEmployee will execute
+        emp3.whoAmI(); 
 
         // Explanation:
         // emp3.whoAmI() prints "I am a PartTimeEmployee" because the actual object
         // is a PartTimeEmployee, even though it's referenced as an Employee due to upcasting.
         // emp3.getHourlyRate(); causes a syntax error because Employee class does not have a method called getHourlyRate.
 
-        // Testing if emp3 is an instance of PartTimeEmployee
+        
         if (emp3 instanceof PartTimeEmployee) {
             System.out.println("emp3 instance of part time employee"); // Printed
         } else {
@@ -40,9 +40,9 @@ public class DemoEmployee {
 
         // Downcasting 2 (Correct Downcasting with instanceof check)
         if (emp3 instanceof PartTimeEmployee) {
-            System.out.println("emp3 instance of part time employee"); // Printed
-            PartTimeEmployee pEmp4 = (PartTimeEmployee) emp3; // Safe cast because emp3 is indeed a PartTimeEmployee
-            System.out.println(pEmp4.getHourlyRate()); // This will print the hourly rate of pEmp4 (emp3 upcasted to Employee)
+            System.out.println("emp3 instance of part time employee"); 
+            PartTimeEmployee pEmp4 = (PartTimeEmployee) emp3; 
+            System.out.println(pEmp4.getHourlyRate()); 
         } else {
             System.out.println("emp3 not instance of part time employee");
         }
