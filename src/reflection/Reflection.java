087727e87258643dbce7d4dbf53a5123fdd7bb0e
K-ZX <163035740+K-ZX@users.noreply.github.com>
2024-10-07 18:37:25 -0400

    Update Reflection.java

--- src/reflection/Reflection.java
+++ src/reflection/Reflection.java
@@ -3,18 +3,14 @@ package reflection;
 import java.lang.reflect.Method;
 
 /**
- * @author ADD YOUR NAME
+ * @author Omar Alcantar
  *
  */
 
-// Demonstration of Reflection
+
 public class Reflection {
 
-    /**
-     * Print the class corresponding to the object
-     * 
-     * @param o the object
-     */
+
     public void correspondingClass(Object o) {
         if (o == null)
             throw new IllegalArgumentException("Object passed is null");
@@ -22,13 +18,7 @@ public class Reflection {
         System.out.println("Inspecting class: " + o.getClass().getName());
     }
 
-    /**
-     * Print the chain of super classes of the object Format of the output:
-     * Inheritance chain: Cn inherits from Cn-1 inherits ... inherits from
-     * java.lang.Object
-     * 
-     * @param o the object
-     */
+
     public void inheritanceChain(Object o) {
         if (o == null)
             throw new IllegalArgumentException("Object passed is null");
@@ -45,47 +35,37 @@ public class Reflection {
         System.out.println();
     }
 
-    /**
-     * Print the list of methods of the object
-     * 
-     * @param o an object
-     */
+
     public void listMethods(Object o) {
         if (o == null)
             throw new IllegalArgumentException("Object passed is null");
 
         Method[] methods = o.getClass().getMethods();
 
-        // List of methods
+       
         System.out.println("\nList of methods:");
         for (Method method : methods) {
             System.out.println(method.getName());
         }
     }
 
-    /**
-     * Constructor
-     */
+
     public Reflection() {
     }
 
-    /**
-     * Demonstration
-     * 
-     * @param args
-     */
+
     public static void main(String[] args) {
 
         Reflection r = new Reflection();
 
-        // Demonstration of the methods on an object of type String
+        
         String str = "Hello, Reflection!";
         System.out.println("String object:");
         r.correspondingClass(str);
         r.inheritanceChain(str);
         r.listMethods(str);
 
-        // Demonstration of the methods on an object of type ColoredCircle
+        
         ColoredCircle cc = new ColoredCircle(0, 0, 10, "Red");
         System.out.println("\nColoredCircle object:");
         r.correspondingClass(cc);
@@ -94,7 +74,7 @@ public class Reflection {
     }
 }
 
-// Example class for demonstration
+
 class ColoredCircle {
     private int x, y;
     private int radius;
